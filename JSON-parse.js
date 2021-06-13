f92927877dd4e6a424c4f41ef450c2e3edaacc91
Abbu Tulu <atullu1234@gmail.com>
2021-06-13 15:57:52 -0400

    Won't parse JSON-parse.js

--- JSON-parse.js
+++ JSON-parse.js
@@ -1,30 +1,29 @@
-const student = `{
-    name: "Johnny",
-    age: 19,
-    isActive: true,
-  }`;
-
-const alumni = `[
-    {
-      name: "Abbu",
-      age: 20,
-      job: "front end developer",
-      isActive: true,
-      hobbies: ["Working out, gaming, pc building", "coding"],
-      friends: [
-        {
-          name: "AJ",
-          age: 18,
-          job: "Entrepreneur",
-        },
-        {
-          name: "Abdulkahar",
-          age: 19,
-          job: "Security guard",
-        },
-      ],
-    },
-  ]`;
-
+let student = `{
+  name: "Johnny",
+  age: 19,
+  isActive: true,
+}`;
 console.log(JSON.parse(student));
+
+let alumni = `[
+  {
+    name: "Abbu",
+    age: 20,
+    job: "front end developer",
+    isActive: true,
+    hobbies: ["Working out, gaming, pc building", "coding"],
+    friends: [
+      {
+        name: "AJ",
+        age: 18,
+        job: "Entrepreneur",
+      },
+      {
+        name: "Abdulkahar",
+        age: 19,
+        job: "Security guard",
+      },
+    ],
+  },
+]`;
 console.log(JSON.parse(alumni));
